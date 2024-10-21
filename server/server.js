2c03a8856eef390a0c06407415aa916311c6b619
kyrcb <johndavidlaureles@gmail.com>
2024-10-21 22:23:58 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -160,6 +160,7 @@ setInterval(updateEnemyPosition, 100);
 
 
 // Start the server
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
